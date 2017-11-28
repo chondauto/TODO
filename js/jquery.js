ee46e0cefc95fd90c255ef84d0f5c4233d0735cd
chondauto <truongchon02031998@gmail.com>
2017-11-28 08:42:06 +0700

    jquery homework

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+$( document ).keypress(function(e) {
+    var input = $('.input');
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if( keycode == '13'){
+        $('<p style = "font-size: 25px; border-bottom: 1px solid gray">'+input.val()+'</p>').appendTo('div.test'); 
+        $(input).val('');
+}})
